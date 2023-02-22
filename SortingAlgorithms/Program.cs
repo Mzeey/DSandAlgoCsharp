67d129b26279bb5ae63edf3cbf161b6faceed56b
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2023-02-22 20:17:48 +0000

    Implements Merge Sort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -5,7 +5,7 @@ public class Program{
         int[] A = {3,5,8,9,6,2};
         Console.Write($"Original Array: ");
         sort.Display(A);
-        sort.ShellSort(A, A.Length);
+        sort.MergeSort(A, 0, A.Length -1);
         Console.Write("Sorted Array: ");
         sort.Display(A);
     }
--- SortingAlgorithms/Sort.cs
+++ SortingAlgorithms/Sort.cs
@@ -7,11 +7,15 @@ namespace SortingAlgorithms
 {
     public class Sort
     {
-        public void SelectionSort(int[] A, int n){
-            for(int i = 0; i < n-1; i++){
+        public void SelectionSort(int[] A, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
                 int position = i;
-                for(int j = i+1; j< n; j++){
-                    if(A[j] < A[position]){
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (A[j] < A[position])
+                    {
                         position = j;
                     }
                 }
@@ -20,58 +24,123 @@ namespace SortingAlgorithms
         }
 
         // n(o^2)
-        public void InsersionSort(int[] A, int n){
-            for (int i = 1; i < n; i++){ 
+        public void InsersionSort(int[] A, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
                 int currentValue = A[i];
                 int position = i;
-                while( position > 0 && A[position-1] > currentValue){
-                    swap(A, position, position-1);
-                    position = position -1;
+                while (position > 0 && A[position - 1] > currentValue)
+                {
+                    swap(A, position, position - 1);
+                    position = position - 1;
                 }
             }
         }
 
-        public void BubbleSort(int[] A, int n){
+        public void BubbleSort(int[] A, int n)
+        {
             int i = 1;
-            while(n > 0){
-                for(i =0; i < n -1; i++ ){
-                    if(A[i+1] < A[i]){
-                        swap(A,i, i+1);
+            while (n > 0)
+            {
+                for (i = 0; i < n - 1; i++)
+                {
+                    if (A[i + 1] < A[i])
+                    {
+                        swap(A, i, i + 1);
                     }
                 }
-                n = n-1;
+                n = n - 1;
             }
         }
 
-        public void ShellSort(int[] A, int n ){
-            int gap = n /2;
-            while(gap > 0){
+        public void ShellSort(int[] A, int n)
+        {
+            int gap = n / 2;
+            while (gap > 0)
+            {
                 int i = gap;
-                while(i < n){
+                while (i < n)
+                {
                     int temp = A[i];
                     int j = i - gap;
-                    while(j >=0 && A[j] > temp){
-                        A[j+gap] = A[j];
-                        j = j -gap;
+                    while (j >= 0 && A[j] > temp)
+                    {
+                        A[j + gap] = A[j];
+                        j = j - gap;
                     }
                     A[j + gap] = temp;
                     i = i + 1;
                 }
 
-                gap = gap/2;
+                gap = gap / 2;
             }
         }
 
-        private void swap(int[] A, int i, int position){
+        public void MergeSort(int[] A, int leftIndex, int rightIndex)
+        {
+            if (leftIndex < rightIndex)
+            {
+                int midIndex = (leftIndex + rightIndex) / 2;
+                MergeSort(A, leftIndex, midIndex); //Splitting of the left halves of the array till it's lowest from
+                MergeSort(A, midIndex + 1, rightIndex); //Splitting of the right halves of the array till it's lowest from
+                merge(A, leftIndex, midIndex, rightIndex);
+            }
+        }
+
+        private void merge(int[] A, int leftIndex, int midIndex, int rightIndex)
+        {
+            int leftArrayIndex = leftIndex;
+            int rightArrayIndex = midIndex + 1;
+            int currentIndex = leftIndex;
+            int[] MergedArray = new int[rightIndex + 1];
+            while (leftArrayIndex <= midIndex && rightArrayIndex <= rightIndex)
+            {
+                if (A[leftArrayIndex] < A[rightArrayIndex])
+                {
+                    MergedArray[currentIndex] = A[leftArrayIndex];
+                    leftArrayIndex++;
+                }
+                else
+                {
+                    MergedArray[currentIndex] = A[rightArrayIndex];
+                    rightArrayIndex++;
+                }
+                currentIndex++;
+            }
+
+            while (leftArrayIndex <= midIndex)
+            {
+                MergedArray[currentIndex] = A[leftArrayIndex];
+                leftArrayIndex++;
+                currentIndex++;
+            }
+
+            while (rightArrayIndex <= rightIndex)
+            {
+                MergedArray[currentIndex] = A[rightArrayIndex];
+                rightArrayIndex++;
+                currentIndex++;
+            }
+
+            for(int x = leftIndex; x <= rightIndex; x++){
+                A[x] = MergedArray[x];
+            }
+        }
+
+        private void swap(int[] A, int i, int position)
+        {
             int temp = A[i];
             A[i] = A[position];
             A[position] = temp;
         }
 
-        public void Display(int[] A){
+        public void Display(int[] A)
+        {
             string result = "";
-            foreach(var item in A){
-                result += (item  + " ");
+            foreach (var item in A)
+            {
+                result += (item + " ");
             }
             Console.WriteLine(result);
         }
