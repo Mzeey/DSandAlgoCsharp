9224dbea518f7999ca10e8d3a04c064209b8d53d
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2023-02-24 00:46:44 +0000

    Adds LinkedList and it's various functionalities

---
+++ LinkedListLearning/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedListLearning;
+
+
+LList<int> MyLinkedList = new LList<int>(78);
+Console.Write("Linked List Values: ");
+MyLinkedList.InsertSorted(7);
+MyLinkedList.InsertSorted(4);
+MyLinkedList.InsertSorted(50);
+MyLinkedList.InsertSorted(12);
+
+MyLinkedList.Display();
+Console.WriteLine("Size: " + MyLinkedList.Size);
+Console.WriteLine("Found at last Index --> " + MyLinkedList.FindAtIndex(MyLinkedList.Size-1));
